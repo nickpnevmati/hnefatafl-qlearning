5369fa377704a8c3dfb32cd25a93c05512c76fd4
Nick Pnevmati <nickpnevmati@gmail.com>
2026-02-20 22:05:40 +0200

    Fri Feb 20 10:05:40 PM EET 2026

--- qlearning/metrics/fix-axes.py
+++ qlearning/metrics/fix-axes.py
@@ -6,13 +6,9 @@ def reset_game_ids_in_jsonl(file_path):
         lines = f.readlines()
     new_lines = []
     for idx, line in enumerate(lines):
-        try:
-            # Try to load only the first JSON object in the line
-            data = json.loads(line)
-            data['game_id'] = idx
-            new_lines.append(json.dumps(data) + '\n')
-        except json.JSONDecodeError:
-            print(f"Warning: Could not parse line {idx} in {file_path}. Skipping.")
+        data = json.loads(line)
+        data['game_id'] = idx
+        new_lines.append(json.dumps(data) + '\n')
     with open(file_path, 'w') as f:
         f.writelines(new_lines)
 
